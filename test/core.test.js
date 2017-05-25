5ddddc0be0885a3d1446b8ecad66a2a4e25a2c42
Raoul v. R <vanruesc@outlook.de>
2017-05-25 17:17:30 +0200

    Improved tests.

--- test/core.test.js
+++ test/core.test.js
@@ -35,13 +35,18 @@ module.exports = {
 
 			const detector = new lib.Detector();
 
-			const TEST_ID = "test";
-			const testFeature = new lib.Feature("Test");
+			const TEST_ID1 = "test1";
+			const TEST_ID2 = "test2";
+			const testFeature1 = new lib.Feature("Test 1");
+			const testFeature2 = new lib.Feature("Test 2");
 
-			testFeature.supported = false;
-			detector.set(TEST_ID, testFeature);
+			testFeature1.supported = false;
+			testFeature2.supported = true;
+			detector.set(TEST_ID1, testFeature1);
+			detector.set(TEST_ID2, testFeature2);
 
 			test.ok(detector.getMissingFeatures().length > 0, "should return a list containing at least one missing feature");
+			test.equal(detector.getMissingFeatures(TEST_ID1, TEST_ID2)[0].supported, false, "should return a list containing exactly one missing feature");
 			test.done();
 
 		},
@@ -50,13 +55,18 @@ module.exports = {
 
 			const detector = new lib.Detector();
 
-			const TEST_ID = "test";
-			const testFeature = new lib.Feature("Test");
+			const TEST_ID1 = "test1";
+			const TEST_ID2 = "test2";
+			const testFeature1 = new lib.Feature("Test 1");
+			const testFeature2 = new lib.Feature("Test 2");
 
-			testFeature.supported = true;
-			detector.set(TEST_ID, testFeature);
+			testFeature1.supported = false;
+			testFeature2.supported = true;
+			detector.set(TEST_ID1, testFeature1);
+			detector.set(TEST_ID2, testFeature2);
 
 			test.ok(detector.getSupportedFeatures().length > 0, "should return a list containing at least one supported feature");
+			test.equal(detector.getSupportedFeatures(TEST_ID1, TEST_ID2)[0].supported, true, "should return a list containing exactly one supported feature");
 			test.done();
 
 		}
