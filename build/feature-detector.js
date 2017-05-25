2a5545bb4fbd2e7e3615fe59c9e07b61a6c0ce9a
Raoul v. R <vanruesc@outlook.de>
2017-05-25 17:19:00 +0200

    Version 0.1.1.

--- build/feature-detector.js
+++ build/feature-detector.js
@@ -1,5 +1,5 @@
 /**
- * feature-detector v0.1.0 build May 25 2017
+ * feature-detector v0.1.1 build May 25 2017
  * https://github.com/vanruesc/feature-detector
  * Copyright 2017 Raoul van Rüschen, Zlib
  */
