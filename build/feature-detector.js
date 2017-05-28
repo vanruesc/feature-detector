c36230bbb50e22579c7aa7fb4d28433b163d9b95
Raoul v. R <vanruesc@outlook.de>
2017-05-28 16:14:37 +0200

    Fresh build.

--- build/feature-detector.js
+++ build/feature-detector.js
@@ -1,5 +1,5 @@
 /**
- * feature-detector v0.1.1 build May 25 2017
+ * feature-detector v0.1.1 build May 28 2017
  * https://github.com/vanruesc/feature-detector
  * Copyright 2017 Raoul van Rüschen, Zlib
  */
@@ -92,62 +92,18 @@
     return call && (typeof call === "object" || typeof call === "function") ? call : self;
   };
 
-  /**
-   * An abstract feature.
-   */
-
   var Feature = function () {
-
-  	/**
-    * Constructs a new feature.
-    *
-    * @param {String} [name=null] - The name of the feature.
-    */
-
   	function Feature() {
   		var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
   		classCallCheck(this, Feature);
 
 
-  		/**
-     * The name of this feature.
-     *
-     * @type {String}
-     */
-
   		this.name = name;
-
-  		/**
-     * The global scope of the current environment.
-     *
-     * This field assumes one of the following values depending on the
-     * environment:
-     *  - browser: window
-     *  - worker: self
-     *  - node.js: global
-     *  - unknown: this
-     *
-     * @type {Object}
-     * @protected
-     */
 
   		this.root = (typeof self === "undefined" ? "undefined" : _typeof(self)) === "object" && self.self === self ? self : (typeof global === "undefined" ? "undefined" : _typeof(global)) === "object" && global.global === global ? global : this;
 
-  		/**
-     * Indicates whether this feature is supported.
-     *
-     * @type {Boolean}
-     * @default false
-     */
-
   		this.supported = false;
   	}
-
-  	/**
-    * Returns a string representation of this object.
-    *
-    * @return {String} The string representation.
-    */
 
   	createClass(Feature, [{
   		key: "toString",
@@ -159,17 +115,6 @@
   	return Feature;
   }();
 
-  /**
-   * An enumeration of feature ids.
-   *
-   * @type {Object}
-   * @property {String} CANVAS - The identifier of the Canvas feature.
-   * @property {String} FILE - The identifier of the File feature.
-   * @property {String} TYPED_ARRAY - The identifier of the Typed Array feature.
-   * @property {String} WEBGL - The identifier of the WebGL feature.
-   * @property {String} WORKER - The identifier of the Web Worker feature.
-   */
-
   var FeatureId = {
 
     CANVAS: "feature.canvas",
@@ -180,25 +125,11 @@
 
   };
 
-  /**
-   * The canvas feature.
-   */
-
   var CanvasFeature = function (_Feature) {
   	inherits(CanvasFeature, _Feature);
 
-  	/**
-    * Constructs a new Canvas feature.
-    */
-
   	function CanvasFeature() {
   		classCallCheck(this, CanvasFeature);
-
-  		/**
-     * Indicates whether the Canvas feature is supported.
-     *
-     * @type {Boolean}
-     */
 
   		var _this = possibleConstructorReturn(this, (CanvasFeature.__proto__ || Object.getPrototypeOf(CanvasFeature)).call(this, "Canvas"));
 
@@ -210,25 +141,11 @@
   	return CanvasFeature;
   }(Feature);
 
-  /**
-   * The File API feature.
-   */
-
   var FileFeature = function (_Feature) {
   	inherits(FileFeature, _Feature);
 
-  	/**
-    * Constructs a new File feature.
-    */
-
   	function FileFeature() {
   		classCallCheck(this, FileFeature);
-
-  		/**
-     * Indicates whether the File API is supported.
-     *
-     * @type {Boolean}
-     */
 
   		var _this = possibleConstructorReturn(this, (FileFeature.__proto__ || Object.getPrototypeOf(FileFeature)).call(this, "File"));
 
@@ -240,25 +157,11 @@
   	return FileFeature;
   }(Feature);
 
-  /**
-   * The Typed Array feature.
-   */
-
   var TypedArrayFeature = function (_Feature) {
   	inherits(TypedArrayFeature, _Feature);
 
-  	/**
-    * Constructs a new Typed Array feature.
-    */
-
   	function TypedArrayFeature() {
   		classCallCheck(this, TypedArrayFeature);
-
-  		/**
-     * Indicates whether the Typed Array feature is supported.
-     *
-     * @type {Boolean}
-     */
 
   		var _this = possibleConstructorReturn(this, (TypedArrayFeature.__proto__ || Object.getPrototypeOf(TypedArrayFeature)).call(this, "Typed Array"));
 
@@ -270,25 +173,11 @@
   	return TypedArrayFeature;
   }(Feature);
 
-  /**
-   * The WebGL feature.
-   */
-
   var WebGLFeature = function (_Feature) {
   		inherits(WebGLFeature, _Feature);
 
-  		/**
-     * Constructs a new WebGL feature.
-     */
-
   		function WebGLFeature() {
   				classCallCheck(this, WebGLFeature);
-
-  				/**
-       * Indicates whether the WebGL feature is supported.
-       *
-       * @type {Boolean}
-       */
 
   				var _this = possibleConstructorReturn(this, (WebGLFeature.__proto__ || Object.getPrototypeOf(WebGLFeature)).call(this, "WebGL"));
 
@@ -321,25 +210,11 @@
   		return WebGLFeature;
   }(Feature);
 
-  /**
-   * The Web Worker feature.
-   */
-
   var WorkerFeature = function (_Feature) {
   	inherits(WorkerFeature, _Feature);
 
-  	/**
-    * Constructs a new Worker feature.
-    */
-
   	function WorkerFeature() {
   		classCallCheck(this, WorkerFeature);
-
-  		/**
-     * Indicates whether the Worker feature is supported.
-     *
-     * @type {Boolean}
-     */
 
   		var _this = possibleConstructorReturn(this, (WorkerFeature.__proto__ || Object.getPrototypeOf(WorkerFeature)).call(this, "Web Worker"));
 
@@ -351,34 +226,10 @@
   	return WorkerFeature;
   }(Feature);
 
-  /**
-   * A collection of features.
-   *
-   * @module features
-   */
-
-  /**
-   * Detects whether certain features are supported in the current environment.
-   */
-
   var Detector = function () {
-
-  	/**
-    * Constructs a new Detector.
-    */
-
   	function Detector() {
   		classCallCheck(this, Detector);
 
-
-  		/**
-     * A feature map.
-     *
-     * Each Feature in the map can be retrieved using its respective FeatureId.
-     * Custom Features may also be added to this map.
-     *
-     * @type {Map}
-     */
 
   		this.features = new Map();
 
@@ -388,17 +239,6 @@
   		this.features.set(FeatureId.WEBGL, new WebGLFeature());
   		this.features.set(FeatureId.WORKER, new WorkerFeature());
   	}
-
-  	/**
-    * Creates a list of either missing or supported features.
-    *
-    * This search can be limited to a subset of features.
-    *
-    * @private
-    * @param {Boolean} missing - Whether only missing features should be returned.
-    * @param {FeatureId[]} featureIds - The ids of the features that you are interested in. If none are supplied, all features will be checked.
-    * @return {Feature[]} A list of either missing or supported features.
-    */
 
   	createClass(Detector, [{
   		key: "getFeatures",
@@ -475,16 +315,6 @@
 
   			return features.length > 0 ? features : null;
   		}
-
-  		/**
-     * Returns a list of missing features.
-     *
-     * This search can be limited to a subset of features.
-     *
-     * @param {...FeatureId} featureIds - The ids of the features that you are interested in. If none are supplied, all features will be checked.
-     * @return {Feature[]} A list of missing features or null if there are none.
-     */
-
   	}, {
   		key: "getMissingFeatures",
   		value: function getMissingFeatures() {
@@ -494,16 +324,6 @@
 
   			return this.getFeatures(true, featureIds);
   		}
-
-  		/**
-     * Returns a list of supported features.
-     *
-     * This search can be limited to a subset of features.
-     *
-     * @param {...FeatureId} featureIds - The ids of the features that you are interested in. If none are supplied, all features will be checked.
-     * @return {Feature[]} A list of supported features or null if there are none.
-     */
-
   	}, {
   		key: "getSupportedFeatures",
   		value: function getSupportedFeatures() {
@@ -513,47 +333,18 @@
 
   			return this.getFeatures(false, featureIds);
   		}
-
-  		/**
-     * Retrieves the requested feature.
-     *
-     * This is a shortcut to the get method of the features map.
-     *
-     * @param {FeatureId} featureId - The id of the feature.
-     * @return {Feature} The requested feature or undefined if it doesn't exist.
-     */
-
   	}, {
   		key: "get",
   		value: function get$$1(featureId) {
 
   			return this.features.get(featureId);
   		}
-
-  		/**
-     * Defines a custom feature.
-     *
-     * This is a shortcut to the set method of the features map.
-     *
-     * @param {FeatureId} featureId - The id of the feature.
-     * @param {Feature} feature - The feature.
-     * @return {Map} The feature map.
-     */
-
   	}, {
   		key: "set",
   		value: function set$$1(featureId, feature) {
 
   			return this.features.set(featureId, feature);
   		}
-
-  		/**
-     * Returns a human-readable info message for the given feature.
-     *
-     * @param {Feature} feature - A feature.
-     * @return {String} The info message.
-     */
-
   	}, {
   		key: "getMessage",
   		value: function getMessage(feature) {
@@ -563,18 +354,6 @@
   	}]);
   	return Detector;
   }();
-
-  /**
-   * Core components.
-   *
-   * @module core
-   */
-
-  /**
-   * Exposure of the library components.
-   *
-   * @module feature-detector
-   */
 
   exports.Detector = Detector;
   exports.Feature = Feature;
